26fc8d91d4b9b2a90833bcfa100333ccb6f9d14c
smoreno11 <60587113+smoreno11@users.noreply.github.com>
2024-01-24 20:54:00 -0800

    Text File
    Automatically reads the file and outputs the data

--- A2_FormattedOutput_InputFile/CreditCardTransactions.java
+++ A2_FormattedOutput_InputFile/CreditCardTransactions.java
@@ -1,5 +1,22 @@
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.Scanner; 
+
 class CreditCardTransactions {
   public static void main(String[] args) {
-      System.out.println("Hello, World!");
+
+    try {
+      File myObj = new File("credit_transactions.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    
   }
 }
