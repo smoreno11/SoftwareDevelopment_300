496d0e4147e9cbadc8b4201b6399ae42f8dae445
smoreno11 <60587113+smoreno11@users.noreply.github.com>
2024-03-04 20:02:40 -0800

    Vacation
    Vacation Class is done.

---
+++ A4_OOP#2/Vacation.java
@@ -0,0 +1,81 @@
+public abstract class Vacation{
+  private String destination;
+  private double budget;
+
+  Vacation()
+  {
+
+  }
+
+  Vacation(String destination, double budget)
+  {
+    setDestination(destination);
+    setBudget(budget);
+  }
+
+  public void setDestination(String destination)
+  {
+    this.destination = destination;
+  }
+
+  public String getDestination(String destination)
+  {
+    return destination;
+  }
+
+  public void setBudget(double budget)
+  {
+    if(budget >= 0)
+    {
+      this.budget = budget;
+    }
+    else
+    {
+      throw new IllegalArgumentException( "Budget must be >= 0.0");
+    }
+  }
+
+  public double getBudger()
+  {
+    return budget;
+  }
+
+  public String toString()
+  {
+    return "Destination: " + destination 
+         + "; Budget: $" + String.format("%.2f",budget) + "\n";
+  }
+
+  public boolean equals(Object obj)
+  {
+    if(this == obj)
+    {
+      return true;
+    }
+
+    if (obj == null || getClass() != obj.getClass())
+    {
+      return false;
+    }
+
+    Vacation tempObject = (Vacation) obj;
+
+    if(destination != null)
+    {
+      if(!destination.equalsIgnoreCase(tempObject.destination))
+      {
+        return false;
+      }
+    }
+    else
+    {
+      if(tempObject.destination != null)
+      {
+        return false;
+      }
+    }
+    
+    return Math.abs(budget - tempObject.budget) < 0.0001;
+  }
+
+}
