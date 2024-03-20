6365c58d9a740ed24f5997aa10c7e34b404d35cf
smoreno11 <60587113+smoreno11@users.noreply.github.com>
2024-03-20 13:07:22 -0700

    Class
    Finished Reverse List and Generic Method

--- A5_ArrayList_Generic/ReverseOrderIntegersWithArrayList.java
+++ A5_ArrayList_Generic/ReverseOrderIntegersWithArrayList.java
@@ -1,6 +1,76 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 public class ReverseOrderIntegersWithArrayList{
-  public static void main(String args){
+  public static void main(String[] args){
     
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print("Please enter the file name containing a list of integers: ");
+    String intFile = keyboard.nextLine();
+    keyboard.close();
+
+    File inputFile = new File(intFile);
+
+    ArrayList<Integer> integerList = new ArrayList<>();
+
+    if (!inputFile.exists())
+    {
+      System.out.println("File does not exitst");
+    }
+    else if (inputFile.length() == 0)
+    {
+      System.out.println("File is empty");
+    }
+    else
+    {
+      try {
+        keyboard = new Scanner(new File(intFile));
+        while(keyboard.hasNextInt())
+        {
+          int count = keyboard.nextInt();
+          listSort(integerList, count);
+          System.out.println(count);
+        }
+        keyboard.close();
+
+        System.out.println("Output Using Enhanced For:");
+        for(Integer go: integerList)
+        {
+          System.out.println(go);
+        }
+
+        System.out.println("Output Using The Iterator:");
+        for(int i = 0; i < integerList.size(); i++)
+        {
+          System.out.println(integerList.get(i));
+        }
+
+      } 
+      catch (FileNotFoundException bad)
+      {
+        System.out.println("ERROR! could not input ");
+      }
+    }
+
   }
+
+  public static void listSort(ArrayList<Integer> list, int num)
+  {
+    if(list.isEmpty() || num >= list.get(0))
+    {
+      list.add(0,num);
+    }
+    else 
+    {
+      int i = 0;
+      while(i< list.size() && num < list.get(i))
+      {
+        i++;
+      }
+      list.add(i,num);
+    }
+  }
+
 }
---
+++ A6_IntroToGenerics/OverloadedMethods.java
@@ -0,0 +1,30 @@
+// Printing array elements using overloaded methods.
+
+public class OverloadedMethods 
+{ 
+	// method printArray to print Integer array 
+
+	public static <T> void printArray(T[] inputArray) 
+	{ 
+		// display array elements 	
+		for (T element : inputArray) 
+		{ 
+			System.out.printf("%s ", element); 	
+		} 		
+		System.out.printf("\n"); 	
+	} 
+	
+	public static void main(String[] args) 
+	{ 
+		// create arrays of Integer, Double and Character 
+		Integer[] integerArray =  {1, 2, 3, 4, 5, 6}; 
+		Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7}; 
+		Character[] characterArray = {'H', 'E', 'L', 'L', 'O'}; 
+		System.out.printf("Array integerArray contains: "); 
+		printArray(integerArray); //pass an Integer array 
+		System.out.printf("Array doubleArray contains: "); 
+		printArray(doubleArray); //pass a Double array 
+		System.out.printf("Array characterArray contains: "); 
+		printArray(characterArray); // pass a Character array 
+	} 
+}
